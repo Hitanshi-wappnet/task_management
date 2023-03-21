0c0ede93f61761038e7523e3f67a417b8f0fb5f1
Hitanshi-wappnet <hitanshi.wappnet@gmail.com>
2023-03-21 16:33:09 +0530

    done verify otp in registration

--- authentication/urls.py
+++ authentication/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from authentication import views
 
 urlpatterns = [
-    path("register/", views.RegisterView.as_view(), name='register')
+    path("register/", views.RegisterView.as_view(), name='register'),
+    path("verifyotp/", views.VerifyOtpView.as_view(), name='verifyotp')
 ]
